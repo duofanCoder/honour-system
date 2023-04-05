920db5ee5e66236fcc9294ee318c8b67dcee7336
duofanHomePC <2441051071@qq.com>
2023-04-05 18:37:42 +0800

    feat: ok

--- src/service/api/file.ts
+++ src/service/api/file.ts
@@ -7,8 +7,22 @@ export function fetchFileUpload(data: FormData) {
 
 export function fetchFileDownload(data: number) {
   if ((data = 1)) {
-    request.get('/file/download/user', {});
+    request.post('/file/download/user', {}, { responseType: 'blob' }).then((response) => {
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', 'user.xlsx'); //or any other extension
+      document.body.appendChild(link);
+      link.click();
+    });
   } else {
-    request.get('/file/download/honour', {});
+    request.post('/file/download/honour', {}, { responseType: 'blob' }).then((response) => {
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', 'honour.xlsx'); //or any other extension
+      document.body.appendChild(link);
+      link.click();
+    });
   }
 }
